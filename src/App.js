86ff14d29b079c12214d13f166a21ff96acc5468
UseK <use.k.james@gmail.com>
2018-10-20 16:43:05 +0900

    onClickイベントを使ってみる

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function showMessage() {
+  alert("Hello");
+}
+
 class App extends Component {
   render() {
     const name = 'usek';
     return (
       <div>
         <p>Hello! {name.toUpperCase()}</p>
-        <div className='box'/>
+        <div className='box' onClick={showMessage}/>
         <p>aiee</p>
       </div>
     );
